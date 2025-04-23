a69efbb44324443636edc6159fef401c91a89e9d
Mandalore136 <luis.gsepulveda136@gmail.com>
2025-04-23 15:05:06 +0200

    Retry with full screen

--- App/CreateDriver.py
+++ App/CreateDriver.py
@@ -12,7 +12,7 @@ def build_chrome_options():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--maximize")
-    #options.add_argument("--start-fullscreen")
+    options.add_argument("--start-fullscreen")
     options.add_argument("--no-sandbox")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
 
