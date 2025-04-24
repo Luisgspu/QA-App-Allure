83822ac81a2915ccf0beeabb10f8dc763f9bd9cb
Mandalore136 <luis.gsepulveda136@gmail.com>
2025-04-24 10:12:41 +0200

    Using Github Token

--- QAAppAllure.py
+++ QAAppAllure.py
@@ -228,13 +228,7 @@ manual_test_cases = [
         
     },
     
-    {
-        "test_name": "BFV2",
-        "market_code": "IT/it",
-        "model_code": "V177-fl",
-        
-        
-    },
+ 
    
 
     
